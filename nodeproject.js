637a0d8eaf23eda3ff84aec11592096157be6c08
bongsokkang <bongsokkang@gmail.com>
2017-11-27 01:14:28 -0800

    Add files via upload

---
+++ nodeproject.js
@@ -0,0 +1,78 @@
+import { lchmod } from "fs";
+
+var inquirer = require ("inquirer");
+
+var points = 0;
+
+function lives () {
+
+}
+
+function hello (h,e,l,l,o){
+    this.h = h;
+
+
+    this.e = e;
+
+    this.l = l;
+    this.l = l;
+    this.o = o;
+
+    this.function(){
+        if (one === h){
+            console.log (h);
+        }
+        if (two === e){
+            console.log(e);
+        }
+        if (three === l){
+            console.log(l);
+        }
+        if (four === l){
+            console.log("l");
+        }
+        if(five === o){
+            console.log("l");
+        }
+        if( hello)
+            new character
+
+        
+                else (){
+                    console.log("_")
+                };
+    }
+
+hello = new character ("hello");
+}
+
+inquirer.prompt(){
+    type: "input"
+    name: "hello";
+    choices: ["h","e","l","l","o"];
+
+}
+.then(function bottom() {
+    if(name){
+        console.log (this.choices);
+        //put into text
+        //display text
+        var points++;
+    }
+    if (points >= 5){
+        console.log(hello);
+        console.log("you are victorious");
+    }
+    if (choices === l){
+        console.log(l);
+    }
+    if (four === l){
+        console.log("l");
+    }
+    if(five === o){
+        console.log("l");
+    }
+     else (){
+         console.log("_");
+            };
+});
